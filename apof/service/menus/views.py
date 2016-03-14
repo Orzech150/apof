c0d773ad14ec268a5457538895d319d09ff9e686
Jakub Maslanka <Ocean.spokojny@op.pl>
2016-03-14 17:50:37 +0100

    [feature/apof-22] name and dir change

---
+++ apof/service/menus/views.py
@@ -0,0 +1,38 @@
+from django.views.generic import TemplateView, View
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+
+class Index(TemplateView):
+    """
+    View for main page.
+    """
+    template_name = 'menus/index.html'
+
+
+class LoginView(View):
+    """
+    View for logging in to our app.
+    """
+    form_class = AuthenticationForm
+    template_name = 'menus/login.html'
+
+    def get(self, request, form=None):
+        """
+        Displays clear AuthenticationForm on page.
+        """
+        if form is None:
+            form = self.form_class()
+
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        """
+        Log an user into app.
+        """
+        form = self.form_class(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('index')
+        return self.get(request, form)
